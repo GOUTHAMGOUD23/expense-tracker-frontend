54762d5251d96e5e12f34cb68e7f4b14df3a3b8e
Appala Gowtham <190063527+GOUTHAMGOUD23@users.noreply.github.com>
2026-04-14 22:53:58 +0530

    frontend fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+try {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} catch (e) {
+  document.body.innerHTML = "<h1 style='color:red'>APP CRASHED</h1>"
+  console.error(e)
+}
